b547845ae72740efa7bf257437a879c564e28606
unknown <linx0832@umn.edu>
2022-06-24 11:02:31 +0800

    change file paths

--- models/TextRCNN.py
+++ models/TextRCNN.py
@@ -4,22 +4,25 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
+import os
+from os import path
+
 
 class Config(object):
 
     """配置参数"""
     def __init__(self, dataset, embedding):
         self.model_name = 'TextRCNN'
-        self.train_path = dataset + '/data/train.txt'                                # 训练集
-        self.dev_path = dataset + '/data/dev.txt'                                    # 验证集
-        self.test_path = dataset + '/data/test.txt'                                  # 测试集
+        self.train_path = path.dirname(path.dirname(__file__))+'/'+ dataset + '/data/train.txt'                                # 训练集
+        self.dev_path = path.dirname(path.dirname(__file__))+'/'+ dataset + '/data/dev.txt'                                    # 验证集
+        self.test_path = path.dirname(path.dirname(__file__))+'/'+ dataset + '/data/test.txt'                                  # 测试集
         self.class_list = [x.strip() for x in open(
-            dataset + '/data/class.txt', encoding='utf-8').readlines()]              # 类别名单
-        self.vocab_path = dataset + '/data/vocab.pkl'                                # 词表
-        self.save_path = dataset + '/saved_dict/' + self.model_name + '.ckpt'        # 模型训练结果
-        self.log_path = dataset + '/log/' + self.model_name
+            path.dirname(path.dirname(__file__))+'/'+ dataset + '/data/class.txt', encoding='utf-8').readlines()]              # 类别名单
+        self.vocab_path = path.dirname(path.dirname(__file__))+'/'+ dataset + '/data/vocab.pkl'                                # 词表
+        self.save_path = path.dirname(path.dirname(__file__))+'/'+ dataset + '/saved_dict/' + self.model_name + '.ckpt'        # 模型训练结果
+        self.log_path = path.dirname(path.dirname(__file__))+'/'+ dataset + '/log/' + self.model_name
         self.embedding_pretrained = torch.tensor(
-            np.load(dataset + '/data/' + embedding)["embeddings"].astype('float32'))\
+            np.load(path.dirname(path.dirname(__file__))+'/'+ dataset + '/data/' + embedding)["embeddings"].astype('float32'))\
             if embedding != 'random' else None                                       # 预训练词向量
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
 
